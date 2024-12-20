da3ba2860071180c99ed809cc26d9e114a092dd2
nabilamarshasafitri <nabilamarshasafitri@gmail.com>
2024-12-20 16:21:58 +0700

    Update tugasfungsi.cpp

--- tugasfungsi.cpp
+++ tugasfungsi.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+// Fungsi untuk menghitung a^b (a pangkat b)
+int f (int x) {
+    int hasil = x*x - 2*x + 5;
+    return hasil;
+}
+
+int main() {
+    int A, B;;
+
+    // Meminta input dari pengguna
+    cout << "Masukkan nilai A: ";
+    cin >> A;
+    cout << "Masukkan nilai B: ";
+    cin >> B;
+
+    // Memanggil fungsi f dan menampilkan hasilnya
+    for (int x = A; x<= B; x++) {
+        cout <<  "f(" << x << ") = " << f(x) << endl;
+    }
+
+    return 0;
+}
